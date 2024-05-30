8d587df1d5b07ddbe27ae4a01e3076a85c9420a0
Lorenz Schüler <lorenz@saarow.de>
2024-05-30 12:03:55 +0200

    work balancing mandelbrot

--- assignments/02/mandelbrot_mpi.cpp
+++ assignments/02/mandelbrot_mpi.cpp
@@ -15,7 +15,10 @@
 #pragma GCC diagnostic ignored "-Wstringop-overflow"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-#pragma GCC diagnostic push
+#pragma GCC diagnostic pop
+
+#define ASSIGNMENT_TAG 1
+#define DATA_TAG 2
 
 constexpr int default_size_x = 1344;
 constexpr int default_size_y = 768;
@@ -25,9 +28,9 @@ constexpr int num_channels = 3;
 // max iterations cutoff
 constexpr int max_iterations = 10000;
 
-#define IND(Y, X, SIZE_Y, SIZE_X, CHANNEL) (Y * SIZE_X * num_channels + X * num_channels + CHANNEL)
+size_t index(int y, int x, int size_x, int channel) { return y * size_x * num_channels + x * num_channels + channel; }
 
-size_t index(int y, int x, int /*size_y*/, int size_x, int channel) { return y * size_x * num_channels + x * num_channels + channel; }
+size_t lindex(int x, int channel) { return x * num_channels + channel; }
 
 using Image = std::vector<uint8_t>;
 
@@ -64,50 +67,67 @@ auto HSVToRGB(double H, const double S, double V) {
 	return std::make_tuple(R, G, B);
 }
 
-void calcMandelbrotX(Image &image, int size_x, int size_y, int start_y, int end_y) {
+void calcMandelbrot(Image &line, int size_x, int size_y, int pixelY) {
 	const float left = -2.5, right = 1;
 	const float bottom = -1, top = 1;
 
-	for (int pixel_y = start_y; pixel_y < end_y; pixel_y++) {
-		// scale y pixel into mandelbrot coordinate system
-		const float cy = (pixel_y / (float)size_y) * (top - bottom) + bottom;
-		for (int pixel_x = 0; pixel_x < size_x; pixel_x++) {
-			// scale x pixel into mandelbrot coordinate system
-			const float cx = (pixel_x / (float)size_x) * (right - left) + left;
-			float x = 0;
-			float y = 0;
-			int num_iterations = 0;
+	// scale y pixel into mandelbrot coordinate system
+	const float cy = (pixelY / (float)size_y) * (top - bottom) + bottom;
+	for (int pixel_x = 0; pixel_x < size_x; pixel_x++) {
+		// scale x pixel into mandelbrot coordinate system
+		const float cx = (pixel_x / (float)size_x) * (right - left) + left;
+		float x = 0;
+		float y = 0;
+		int num_iterations = 0;
 
-			// Check if the distance from the origin becomes
-			// greater than 2 within the max number of iterations.
-			while ((x * x + y * y <= 2 * 2) && (num_iterations < max_iterations)) {
-				float x_tmp = x * x - y * y + cx;
-				y = 2 * x * y + cy;
-				x = x_tmp;
-				num_iterations += 1;
-			}
-
-			// Normalize iteration and write it to pixel position
-			double value = fabs((num_iterations / (float)max_iterations)) * 200;
-
-			auto [red, green, blue] = HSVToRGB(value, 1.0, 1.0);
-
-			int channel = 0;
-			image[index(pixel_y, pixel_x, size_y, size_x, channel++)] = (uint8_t)(red * UINT8_MAX);
-			image[index(pixel_y, pixel_x, size_y, size_x, channel++)] = (uint8_t)(green * UINT8_MAX);
-			image[index(pixel_y, pixel_x, size_y, size_x, channel++)] = (uint8_t)(blue * UINT8_MAX);
+		// Check if the distance from the origin becomes
+		// greater than 2 within the max number of iterations.
+		while ((x * x + y * y <= 2 * 2) && (num_iterations < max_iterations)) {
+			float x_tmp = x * x - y * y + cx;
+			y = 2 * x * y + cy;
+			x = x_tmp;
+			num_iterations += 1;
 		}
+
+		// Normalize iteration and write it to pixel position
+		double value = fabs((num_iterations / (float)max_iterations)) * 200;
+
+		auto [red, green, blue] = HSVToRGB(value, 1.0, 1.0);
+
+		line[lindex(pixel_x, 0)] = (uint8_t)(red * UINT8_MAX);
+		line[lindex(pixel_x, 1)] = (uint8_t)(green * UINT8_MAX);
+		line[lindex(pixel_x, 2)] = (uint8_t)(blue * UINT8_MAX);
 	}
 }
 
-void calcMandelbrot(Image &image, int size_x, int size_y, int rank, int ranks) {
-	int rows = size_y / ranks;
-	int start_y = rows * rank;
-	int end_y = rows * (rank + 1);
-	calcMandelbrotX(image, size_x, size_y, start_y, end_y);
-	MPI_Gather(&image[start_y * size_x * num_channels], rows * size_x * num_channels, MPI_CHAR, &image[0], rows * size_x * num_channels, MPI_CHAR, 0,
-	           MPI_COMM_WORLD);
+void sendStop(int rank) {
+	int stop = -1;
+	MPI_Send(&stop, 1, MPI_INT, rank, ASSIGNMENT_TAG, MPI_COMM_WORLD);
 }
+
+void sendAssignment(int &sendY, std::vector<int> &lastAssignments, int rank) {
+	MPI_Send(&sendY, 1, MPI_INT, rank, ASSIGNMENT_TAG, MPI_COMM_WORLD);
+	lastAssignments[rank] = sendY;
+	sendY++;
+}
+
+int recvData(int &recvY, std::vector<int> &lastAssignments, std::vector<uint8_t> &image, int size_x) {
+	MPI_Status status;
+	MPI_Probe(MPI_ANY_SOURCE, DATA_TAG, MPI_COMM_WORLD, &status);
+	int rank = status.MPI_SOURCE;
+	int y = lastAssignments[rank];
+	MPI_Recv(&image[index(y, 0, size_x, 0)], size_x * num_channels, MPI_UINT8_T, rank, DATA_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	recvY++;
+	return rank;
+}
+
+int recvAssignment() {
+	int y;
+	MPI_Recv(&y, 1, MPI_INT, 0, ASSIGNMENT_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	return y;
+}
+
+void sendData(std::vector<uint8_t> &line, int size_x) { MPI_Send(&line[0], size_x * num_channels, MPI_UINT8_T, 0, DATA_TAG, MPI_COMM_WORLD); }
 
 int main(int argc, char **argv) {
 	MPI_Init(&argc, &argv);
@@ -127,18 +147,49 @@ int main(int argc, char **argv) {
 		std::cout << "No arguments given, using default size " << size_x << "x" << size_y << std::endl;
 	}
 
-	Image image(num_channels * size_x * size_y);
-
-	auto time_start = std::chrono::high_resolution_clock::now();
-	calcMandelbrot(image, size_x, size_y, rank, ranks);
 	if (rank == 0) {
+		auto image = Image(num_channels * size_x * size_y);
+		auto lastAssignments = std::vector<int>(ranks);
+		int sendY = 0;
+		int recvY = 0;
+
+		auto time_start = std::chrono::high_resolution_clock::now();
+
+		for (int r = 1; r < ranks; r++) {
+			sendAssignment(sendY, lastAssignments, r);
+		}
+
+		while (sendY < size_y) {
+			int recvRank = recvData(recvY, lastAssignments, image, size_x);
+
+			sendAssignment(sendY, lastAssignments, recvRank);
+		}
+
+		for (int r = 1; r < ranks; r++) {
+			sendStop(r);
+		}
+
+		while (recvY < size_y) {
+			recvData(recvY, lastAssignments, image, size_x);
+		}
+
 		auto time_end = std::chrono::high_resolution_clock::now();
 		auto time_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
 
 		std::cout << "Mandelbrot set calculation for " << size_x << "x" << size_y << " took: " << time_elapsed << " ms." << std::endl;
 
 		constexpr int stride_bytes = 0;
-		stbi_write_png("mandelbrot_mpi.png", size_x, size_y, num_channels, image.data(), stride_bytes);
+		stbi_write_png("mandelbrot_mpi.png", size_x, size_y, num_channels, &image[0], stride_bytes);
+	} else {
+		auto line = Image(num_channels * size_x);
+		while (true) {
+			int pixelY = recvAssignment();
+			if (pixelY < 0) { // -1 = stop
+				break;
+			}
+			calcMandelbrot(line, size_x, size_y, pixelY);
+			sendData(line, size_x);
+		}
 	}
 
 	MPI_Finalize();
